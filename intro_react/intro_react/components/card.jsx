8553452179b3a4f4ab6a4e9a73a774f52dc98f3e
desarrollador4@azsmart.net <desarrollador4@azsmart.net>
2025-03-07 07:50:38 -0500

    creating and adding new component

---
+++ intro_react/intro_react/components/card.jsx
@@ -0,0 +1,8 @@
+const Card1 = ({ title, description }) => {
+    return <section>
+        <h2>{title}</h2>
+        <p>{description}</p>
+    </section>
+}
+
+export default Card1; 
--- intro_react/intro_react/src/App.jsx
+++ intro_react/intro_react/src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Card1 from '../components/card'
+
 function App() {
   const items = ["react", "Javascript", "Vite"];
 
   return (
     <section>
       <h1>Hola Carlos</h1>
+      <Card1 title="Card 1" description="Description" />
+      <Card1 title="Card 2" description="Description 2" />
       <ul>
         {
           items.map((item, index) =>
