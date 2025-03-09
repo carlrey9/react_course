01bb0c39f344d8106843e036c070f3745af70ac7
Carlos Rey <crey8640@gmail.com>
2025-03-09 09:43:13 -0500

    adding tailwind css

--- intro_react/my-react-app-styles/src/App.jsx
+++ intro_react/my-react-app-styles/src/App.jsx
@@ -1,12 +1,12 @@
 import Card from "./components/card/card";
 import Button from "./components/Button/Button";
+import Text from "./components/Text/Text";
 
 function App() {
 
   return (
     <>
-      <Button blue >Blue </Button>
-      <Button  >Gray </Button>
+      <Text />
     </>
   )
 }
