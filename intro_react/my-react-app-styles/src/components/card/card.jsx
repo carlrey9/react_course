15930a63066699feedb801a91ed7cd2c65167a2e
Carlos Rey <crey8640@gmail.com>
2025-03-09 07:35:40 -0500

    adding css and sass styles in the project

---
+++ intro_react/my-react-app-styles/src/components/card/card.jsx
@@ -0,0 +1,11 @@
+import styles from "./Card.module.sass";
+
+const Card = () => {
+    return (
+        <div className={styles.card}>
+            Soy una Card
+        </div>
+    );
+};
+
+export default Card;
